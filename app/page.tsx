bfd61575746cc90d839bf05887270e35a07f3d80
Felpasw <felipecavalcantelacerda@hotmail.com>
2024-11-28 20:42:18 -0300

    Ajustado para aplicação para lib nextUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,56 @@
-'use client'
-import Input from "@/components/input";
-import { useState } from "react";
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
 
-
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
- const [email, setEmail]= useState<string>('')
- const [password, setPassword]= useState<string>('')
-
- 
   return (
-  <main className="w-full min-h-[100vh] flex flex-col items-center justify-center gap-12 p-">
-    <Input label={"E-mail"} value={email} onChange={(e) => setEmail(e.target.value)} width={"w-full"} type={"text"} />
-    <Input label={"Password"} value={password} onChange={(e) => setPassword(e.target.value)} width={"w-full"} type={"password"} />
-  
-  
-  </main>      
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>Make&nbsp;</span>
+        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <br />
+        <span className={title()}>
+          websites regardless of your design experience.
+        </span>
+        <div className={subtitle({ class: "mt-4" })}>
+          Beautiful, fast and modern React UI library.
+        </div>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+          href={siteConfig.links.docs}
+        >
+          Documentation
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Snippet hideCopyButton hideSymbol variant="bordered">
+          <span>
+            Get started by editing <Code color="primary">app/page.tsx</Code>
+          </span>
+        </Snippet>
+      </div>
+    </section>
   );
 }
